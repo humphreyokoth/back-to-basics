bb4b6a37ecbbd6729e4182dd6c2f40949f5e341e
humphreyokoth <humpasp@gmail.com>
2022-12-12 09:46:58 +0300

    Feat:sorting

---
+++ sort.c
@@ -0,0 +1,23 @@
+#include<cs50.h>
+#include<stdio.h>
+#include<string.h>
+
+
+int main(void){
+string names []= {"carter","David"};
+string numbers [] = {"+12345959","+170594945"};
+
+for (int i = 0; i < 2; i++)
+{
+    if (strcmp(names[i],"David")== 0)
+    {
+       printf("Found %s\n", numbers[i]);
+       return 0;
+    }
+    
+}
+printf("Not found \n");
+return 1;
+
+
+}
